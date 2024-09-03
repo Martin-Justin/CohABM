42ac2fe39aad78856b62b5d4494aeefe781b4b82
Martin Justin <martin1123581321@gmail.com>
2024-09-03 16:59:59 +0200

    Initial commit

---
+++ CohABM_results.py
@@ -0,0 +1,29 @@
+from CohABM_0_2 import CoherenceModel
+import pandas as pd
+import mesa
+from multiprocessing import freeze_support
+
+
+params = {"N":10,
+           "network": ["complete", "wheel", "cycle"],
+           "BN": "sprinkler",
+           "pulls":10,
+           "agent_type":["CoherenceAgent", "NormalAgent"],
+           "noise":[0.2, 0.5, 0.8],
+           "coherence_style":["shogenji", "og"],
+           "misleading_type": ["small_sample", "big_sprinkler"]}
+
+if __name__ == '__main__':
+    freeze_support()
+    results = mesa.batch_run(
+        CoherenceModel,
+        parameters=params,
+        iterations=30,
+        max_steps=30,
+        number_processes=1,
+        data_collection_period=1,
+        display_progress=True,)
+
+#Export results
+    results_df = pd.DataFrame(results)
+    results_df.to_csv("C:/Users/marti/Documents/coherence_results/results.csv")
