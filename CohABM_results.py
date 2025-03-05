b79f5c9e73a5c443d2e69b43d88697d77fc65b95
Martin Justin <martin1123581321@gmail.com>
2025-03-05 08:48:07 +0100

    Update CohABM_results.py

--- CohABM_results.py
+++ CohABM_results.py
@@ -5,14 +5,14 @@ from multiprocessing import freeze_support
 
 params = {"N":1,
            "network": ["complete"],
-           "BN": "asia",
+           "BN": "sprinkler",
            "pulls":100,
            "agent_type":["CoherenceAgent", "NormalAgent"],
            "noise":[0.05, 0.1, 0.15, 0.2, 0.25, 0.3],
            "coherence_style":["ogPlus", "og", "shogenji"],
-           "misleading_type": ["noisy_data", "big_asia"],
+           "misleading_type": ["noisy_data", "big_sprinkler"],
            "prior": ["approx_true"],
-           "background": ["asia"],
+           "background": ["sprinkler"],
            "distance_from_truth": [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4],
           "experts_ratio": 0,
           "latitude":[0],
@@ -24,11 +24,11 @@ if __name__ == '__main__':
         CoherenceModel,
         parameters=params,
         iterations=30,
-        max_steps=50,
+        max_steps=500,
         number_processes=None,
         data_collection_period=1,
         display_progress=True,)
 
     #Export results
     results_df = pd.DataFrame(results)
-    results_df.to_csv("asia.csv")
+    results_df.to_csv("sprinkler-500-rounds.csv")
